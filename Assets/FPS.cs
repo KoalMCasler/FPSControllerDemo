11fb19ca2d3a43b8783bf71749ffc5be85071d01
Koal M. Casler <71401318+KoalMCasler@users.noreply.github.com>
2023-12-06 12:37:53 -0400

    Sprint now works, jump works but is a little weird

--- Assets/FPS.cs
+++ Assets/FPS.cs
@@ -1,24 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.Common;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
 public class FPS : MonoBehaviour
 {
-    private CharacterController player;
-    public GameObject fpsCamera;
+    [Header("Object References")]
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private CharacterController playerController;
+    [SerializeField]
+    private Camera fpsCamera;
+    [Header("Movement Settings")]
     private Vector3 walkInput;
     private Vector3 moveValue;
-    public int speedMultiplier;
-    public int jumpForce;
+    [SerializeField]
+    private float speedMultiplier;
+    [SerializeField]
+    private float sprintMultiplier;
+    [SerializeField]
+    private int jumpForce;
+    [SerializeField]
     private bool isGrounded;
-    public float gravity;
-    public float currentSpeed;
+    [SerializeField]
+    private float gravity;
+    [SerializeField]
+    private float currentSpeed;
+    [SerializeField]
+    private bool isSprinting;
     private Vector3 speedVector;
+    [Header("Look Settings")]
+    [SerializeField]
+    private float lookSensitivity;
+    [SerializeField]
+    private float upDownLimit;
+    private float verticalRotation;
+    
     // Start is called before the first frame update
     void Start()
     {
-        player = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        playerController = GetComponent<CharacterController>();
+        player = gameObject;
+        fpsCamera = Camera.main;
         if(speedMultiplier < 2 || speedMultiplier > 8)
         {
             speedMultiplier = 2;
@@ -31,18 +60,29 @@ public class FPS : MonoBehaviour
         {
             gravity = -1;
         }
+        if(upDownLimit < 60 || upDownLimit > 80)
+        {
+            upDownLimit = 70;
+        }
+        if(sprintMultiplier <= 0)
+        {
+            sprintMultiplier = 1.5f;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        ManageLook();
         ManageInput();
         SpeedTest();
     }
     void ManageInput()
     {
+        walkInput = new Vector3(Input.GetAxis("Horizontal"),0f,Input.GetAxis("Vertical")).normalized;
+        walkInput = transform.rotation * walkInput;
         // Gets Jump input
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") /*&& isGrounded == true*/)
         {
             moveValue.y = jumpForce;
         }
@@ -50,17 +90,40 @@ public class FPS : MonoBehaviour
         {
             moveValue.y = gravity;
         }
-        // Gets walk input
-        walkInput = new Vector3(Input.GetAxis("Horizontal"),0f,Input.GetAxis("Vertical")).normalized;
-        moveValue.x = walkInput.x * speedMultiplier;
-        moveValue.z = walkInput.z * speedMultiplier;
+        if(Input.GetButton("Sprint"))
+        {
+            isSprinting = true;
+        }
+        if(Input.GetButtonUp("Sprint"))
+        {
+           isSprinting = false;
+        }
+        if(isSprinting == true)
+        {
+            moveValue.x = walkInput.x * (speedMultiplier*sprintMultiplier);
+            moveValue.z = walkInput.z * (speedMultiplier*sprintMultiplier);
+        }
+        if(isSprinting == false)
+        {
+            moveValue.x = walkInput.x * speedMultiplier;
+            moveValue.z = walkInput.z * speedMultiplier;
+        }
         // moves player
-        player.Move(moveValue * Time.deltaTime);
+        playerController.Move(moveValue * Time.deltaTime);
+    }
+    void ManageLook()
+    {
+        float mouseXRotation = Input.GetAxis("Mouse X") * lookSensitivity;
+        float mouseYRotation = Input.GetAxis("Mouse Y");
+        player.transform.Rotate(0f,mouseXRotation,0f);
+        verticalRotation -= mouseYRotation * lookSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation,-upDownLimit,upDownLimit);
+        fpsCamera.transform.localRotation = Quaternion.Euler(verticalRotation,0,0);
     }
     void SpeedTest()
     {
         // Tests player speed
-        speedVector = player.velocity;
+        speedVector = playerController.velocity;
         currentSpeed = speedVector.magnitude;
     }
 }
